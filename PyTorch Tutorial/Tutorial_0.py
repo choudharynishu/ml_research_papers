ca6e1af23988eb5d69e58662c26aed024ba2f213
Nishu Choudhary <nishuchoudhary@gatech.edu>
2023-04-08 20:07:44 -0400

    language prediction model

--- PyTorch Tutorial/Tutorial_0.py
+++ PyTorch Tutorial/Tutorial_0.py
@@ -7,9 +7,9 @@ Soumith Chintala, Suraj Subramanian, Seth Juarez, Cassie Breviu, Dmitry Soshniko
 
 06/16/2022
 '''
-#-----Installation-----#
-#Pip installation command for local installation on MacOS (Type this in Local)
-#pip3 install torch torchvision torchaudio
+# -----Installation-----#
+# Pip installation command for local installation on MacOS (Type this in Local)
+# pip3 install torch torchvision torchaudio
 
 import torch
 
@@ -31,45 +31,46 @@ According to the Docs every dataset contains following keyword arguments
                       check if the file is a valid file
 '''
 
-#-----Import commands-----#
-from torch import nn #most likely to construct simple feed-forward networks
-from torchvision import datasets #from here we are going to extract the FashionMNIST data
-from torch.utils.data import DataLoader #primitive to work with data
+# -----Import commands-----#
+from torch import nn  # most likely to construct simple feed-forward networks
+from torchvision import datasets  # from here we are going to extract the FashionMNIST data
+from torch.utils.data import DataLoader  # primitive to work with data
 from torchvision.transforms import ToTensor
 
-#-----Extracting the FashionMNIST dataset into training and test set-----#
-#This will only download once
+# -----Extracting the FashionMNIST dataset into training and test set-----#
+# This will only download once
 training_data = datasets.FashionMNIST(root="data",
-                                      train=True, #specifies training or test dataset,
-                                      download=True, #downloads the data from the internet if it’s not available at root.
-                                      transform=ToTensor() #specify the feature transformations
+                                      train=True,  # specifies training or test dataset,
+                                      download=True,
+                                      # downloads the data from the internet if it’s not available at root.
+                                      transform=ToTensor()  # specify the feature transformations
                                       )
 testing_data = datasets.FashionMNIST(root="data",
                                      train=False,
                                      download=True,
                                      transform=ToTensor())
-#-----Create an iterator out of the downloaded data-----#
+# -----Create an iterator out of the downloaded data-----#
 batch_size = 64
 
-#Create dataloaders
+# Create dataloaders
 
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(testing_data, batch_size=batch_size)
 
-#Demonstration of what DataLoader does
+# Demonstration of what DataLoader does
 for X, y in test_dataloader:
-    #Here N= Batch-size- specified number of observations
+    # Here N= Batch-size- specified number of observations
     print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     print(y)
     break
 
-#-----Define the Neural Network model-----#
-#Machine to use for computation
+# -----Define the Neural Network model-----#
+# Machine to use for computation
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f"Using {device} machine")
 
-#For making the Neural Network - we define a class that inherits from nn.Module class
+# For making the Neural Network - we define a class that inherits from nn.Module class
 '''
 Useful tidbits about class inheritence 
 class C(B):
@@ -77,51 +78,55 @@ class C(B):
         super().method(arg)    # This does the same thing as: super(C, self).method(arg)
 '''
 
+
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         # nn.Flatten layer to convert each 2D 28x28 image into a contiguous array of 784 pixel values
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-                                              #Linear module applies a linear transformation on the input
-                                              #using its stored weights and biases.
-                                              #This layer receives input from the previous flatten layer
-                                              nn.Linear(in_features=28*28, out_features=512),
-                                              nn.ReLU(),
-                                              nn.Linear(in_features=512, out_features=512),
-                                              nn.ReLU(),
-                                              nn.Linear(in_features=512, out_features=10)
-                                            )
-    #Define forward pass on one particular image
+            # Linear module applies a linear transformation on the input
+            # using its stored weights and biases.
+            # This layer receives input from the previous flatten layer
+            nn.Linear(in_features=28 * 28, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=10)
+        )
+
+    # Define forward pass on one particular image
     def forward(self, x):
-        #print(x.size())
-        x = self.flatten(x) #this basically calls nn.Flatten(x)
-        #print(f"Flattened size, {x.size()}")
-        logits = self.linear_relu_stack(x) #this calls the architecture defined in this attributes
+        # print(x.size())
+        x = self.flatten(x)  # this basically calls nn.Flatten(x)
+        # print(f"Flattened size, {x.size()}")
+        logits = self.linear_relu_stack(x)  # this calls the architecture defined in this attributes
         return logits
 
-model = NeuralNetwork().to(device)
-#print(dir(model))
 
-#-----Training the Neural Network model-----#
-loss_function = nn.CrossEntropyLoss()                      #Loss-function
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)   #Optimization function
+model = NeuralNetwork().to(device)
+# print(dir(model))
+
+# -----Training the Neural Network model-----#
+loss_function = nn.CrossEntropyLoss()  # Loss-function
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)  # Optimization function
+
 
 def train(dataloader, model, loss_function, optimizer):
     size = len(dataloader.dataset)
     model.train()
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
-        #Forward pass
+        # Forward pass
         # Compute prediction error and loss value
         pred = model(X)
-        #print(f"predict is {pred}")
+        # print(f"predict is {pred}")
         loss = loss_function(pred, y)
-        #Backpropagation
-        #Optimizer.zero_grad(): sets the gradient of all optimized Torch.tensors to zero
+        # Backpropagation
+        # Optimizer.zero_grad(): sets the gradient of all optimized Torch.tensors to zero
         optimizer.zero_grad()
         loss.backward()
-        #Optimizer.step() performs a single step(parameter update)
+        # Optimizer.step() performs a single step(parameter update)
         optimizer.step()
 
         if batch % 100 == 0:
@@ -129,6 +134,7 @@ def train(dataloader, model, loss_function, optimizer):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
     return None
+
 
 def test(dataloader, model, loss_function):
     size = len(dataloader.dataset)
@@ -138,22 +144,23 @@ def test(dataloader, model, loss_function):
 
     #  Turn off the grad - mainly for efficiency as we won't be updating any parameters
     with torch.no_grad():
-        for batch, (X,y) in enumerate(dataloader):
+        for batch, (X, y) in enumerate(dataloader):
             pred = model(X)
-            #print(f"Trained model's predicted tensor's size, {pred.size()}")
-            #print(f"Actual y, {y}")
+            # print(f"Trained model's predicted tensor's size, {pred.size()}")
+            # print(f"Actual y, {y}")
             test_loss += loss_function(pred, y)
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
     test_loss /= num_of_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
     return None
 
+
 epochs = 5
 for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
+    print(f"Epoch {t + 1}\n-------------------------------")
     train(train_dataloader, model, loss_function, optimizer)
     test(test_dataloader, model, loss_function)
 
--- char_LLM/model.py
+++ char_LLM/model.py
@@ -8,24 +8,57 @@ import string
 import unicodedata
 import argparse
 
-import numpy as np
-import pandas as pd
+import random
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
 
+# torch.manual_seed(19949)
+# ----------Model Architecture------------ #
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        """
+        Weights automatically initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
+            :math:`k = \frac{1}{\text{in\_features}}`
+        """
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x, last_hidden):
+        combined = torch.cat((x, last_hidden), 1)
+        #hidden = self.i2h(combined)
+        #output = self.i2o(combined)
+        hidden = torch.tanh(self.i2h(combined))
+        output = torch.tanh(self.i2o(combined))
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+# ----------File Extraction------------ #
 def findFiles(path):
     return glob.glob(path)
 
 
+# all_letters is a string containing all alphabets
+# (both lower and uppercases) and whitespace, period,
+# comma, semicolon, and single quotation mark
 all_letters = string.ascii_letters + " .,;'"
 n_letters = len(all_letters)
 
 
 # Reference: https://stackoverflow.com/a/518232/2809427
 def unicodeToAscii(name):
+    """
+    :param name: a name (word) - a unicode string
+    :return: converts the name to plain ascii, e.g. Vàsquez to Vasquez
+    """
     return ''.join(
         c for c in unicodedata.normalize('NFD', name)
         if unicodedata.category(c) != 'Mn'
@@ -38,18 +71,119 @@ print(unicodeToAscii('Vásquez'))
 
 # Convert a unicode string (à, è, ñ)to ASCII
 def readLines(filename):
-    '''
+    """
     :param filename: path to a file with names in a particular language
     :return: names (lines) converted back to ASCII
-    '''
+    """
     lines = open(filename, encoding='utf-8').read().strip().split('\n')
     return [unicodeToAscii(line) for line in lines]
 
 
-languages = []
-language_label = {}
+languages = []  # list of all langugages
+language_label = {}  # dictionary for names in each language e.g., Spa
 for file in findFiles(os.path.join(sys.path[0], 'names/*.txt')):
     language = os.path.splitext(os.path.basename(file))[0]
     languages.append(language)
     lines = readLines(file)
     language_label[language] = lines
+n_languages = len(languages)
+
+# ----------Converting extracted data into Tensors------------ #
+# Code to convert alphabets (ascii) to number -one hot encoding vectors
+def letterToIndex(letter):
+    """
+    :param letter: character (string)
+    :return: character's position in the all_letters vocabulary
+    """
+    return all_letters.find(letter)
+
+
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+
+# Turn an entire name into tensor of shape (name_length, 1, n_letters)
+def lineToTensor(line):
+    """
+    :param line: a name (word) string type
+    :return: a tensor of shape (len(name), 1, n_letters) with each 'column'
+            representing one-hot encoding of a particular letter in the word
+    """
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for i, char in enumerate(line):
+        tensor[i][0][letterToIndex(char)] = 1
+    return tensor
+
+def randomChoice(l):
+    """
+    :param l: List from which a random item is to be
+                selected, here it represent list of all languages
+    :return: random language string type
+    """
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample():
+    """
+    Here,
+    languages : List of all languages
+    line: a surname , string type
+    language tensor: A tensor representing the index of randomly selected language
+    line_tensor: A tensor of shape (1, len(line)) (len(line) = length of surname)
+    :return:
+    """
+    language_i = randomChoice(languages)
+    line = randomChoice(language_label[language_i])
+    language_tensor = torch.tensor([languages.index(language_i)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return language_i, line, language_tensor, line_tensor
+
+# ----------Training Model------------ #
+# Initialize the model
+n_hidden = 128  # convert later to user argument
+rnn = RNN(n_letters, n_hidden, n_languages)
+loss_func = nn.NLLLoss()
+learning_rate = 0.01
+max_iter = 10
+current_loss = 0
+loss_values = []
+
+
+def categoryFromOutput(output):
+    top_n, top_i = output.topk(1)
+    language_i = top_i[0].item()
+    return languages[language_i], language_i
+
+
+def training(language_tensor, line_tensor):
+    hidden = rnn.initHidden()  # get initial hidden state as zero tensor with shape(1, hidden_size)
+
+    # Define an optimizer instead
+    rnn.zero_grad()  # zero-in the grad
+    # line_tensor.size()[0] = surname length
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = loss_func(output, language_tensor)
+    loss.backward()
+
+    #  edit code below for optimizer.step()
+    for parameter in rnn.parameters():
+        # parameter.data._add(self, other, alpha) In-place version of add()
+        # If both alpha and other are specified, each element of other is scaled by alpha before being used.
+        parameter.data.add_(parameter.grad.data, alpha=learning_rate)
+    return output, loss.item()
+
+
+for iter in range(1, max_iter + 1):
+    language_i, line, language_tensor, line_tensor = randomTrainingExample()
+    output, loss = training(language_tensor, line_tensor)
+    #current_loss += loss - plot later
+
+    # Print iter number, loss, name and guess
+    if iter % 1000 == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == language_i else '✗ (%s)' % language_i
+        print(f"iteration: {iter}, loss:{loss}, line:{line}, guess:{guess}, correct:{correct} ")
+        #print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
