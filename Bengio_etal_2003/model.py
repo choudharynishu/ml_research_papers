211ef7db783472c8a332c081abad586072dc9c83
Nishu Choudhary <nishuchoudhary@gatech.edu>
2023-04-15 19:24:07 -0400

    added model architectur

--- Bengio_etal_2003/model.py
+++ Bengio_etal_2003/model.py
@@ -32,7 +32,24 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-# ---------------------------------Build Dataset--------------------#
+# ----------Model Architecture------------ #
+class RNN(nn.Module):
+    def __init__(self, vocab_size, embedding_size, embedding_size2):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        self.linear1 = nn.Linear(vocab_size*embedding_size, embedding_size2)
+        self.linear2 = nn.Linear(embedding_size2, vocab_size)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x = torch.tanh(x)
+        x = self.linear1(x)
+        x = torch.tanh(x)
+        x = self.linear2(x)
+
+        return x
+
+# -----------Build Dataset--------------------#
 """
 ==================   ==============   ==============
 Attributes           Bengio et al.    Current(news)
@@ -53,7 +70,8 @@ Pre-processing steps
 5. Build Vocabulary and get its size
 """
 rare_freq = 3
-context = 4
+context = 4  # Number of words used to predict the next word
+embedding_size = 30
 
 news_words = brown.words(categories='news')
 news_words = [w for w in news_words if w.isalpha()]
@@ -67,10 +85,41 @@ for w in news_words:
 vocabulary = list(vocabulary)
 vocab_size = len(vocabulary)
 
-vocab_idx_dict = {word: i for i, word in enumerate(vocabulary)} #probably inefficient
-
+vocab_idx_dict = {word: i for i, word in enumerate(vocabulary)}  # probably inefficient
+# print(news_words[:10])
 print(f"Total number of words in the news category: {len(news_words)}")
 print(f"vocabulary-size for this text corpus: {vocab_size}")
 
 
+def wordToTensor(word):
+    tensor = torch.zeros(1, vocab_size)
+    tensor[0][vocab_idx_dict[word]] = 1
+    return tensor
 
+
+def wordsToTensor(words):
+    """
+    :param words: list of words of length = context
+    :return: tensor with one-hot encoding for each word
+    """
+    tensor = torch.zeros(len(words), 1, vocab_size)
+    for i, word in enumerate(words):
+        tensor[i][0][vocab_idx_dict[word]] = 1
+    return tensor
+
+
+sample_size = len(news_words[:20]) // (context + 1)
+print(sample_size)
+X = torch.empty((sample_size, context, 1, vocab_size))
+y = torch.empty((sample_size, 1, vocab_size))
+rolling_window = []
+# Only 10 for trial - for testing
+# X.shape = ([len(data), context, 1, vocab_size])
+# y.shape = ([len(data), 1, vocab_size])
+start_index = 0
+i = 0
+while i < sample_size:
+    X[i] = wordsToTensor(news_words[start_index:start_index + context])
+    y[i] = wordToTensor(news_words[start_index + context + 1])
+    start_index += 1
+    i += 1
